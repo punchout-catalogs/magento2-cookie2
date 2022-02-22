2c18b3745a1926cd8795baaf0c792881a4d45933
Tolik Legkyi <>
2022-02-22 15:24:17 +0200

    POC-2661 set session params

---
+++ Framework/Session/Generic.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Punchout\Cookie2\Framework\Session;
+
+/**
+ * Magento session configuration
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class Generic extends \Magento\Framework\Session\Generic
+{
+    /**
+     * Configure session handler and start session
+     *
+     * @throws \Magento\Framework\Exception\SessionException
+     * @return $this
+     */
+    public function start()
+    {
+        $this->updateCookieParams();
+        return parent::start();
+    }
+
+    /**
+     * Renew session id and update session cookie
+     *
+     * @return $this
+     */
+    public function regenerateId()
+    {
+        $this->updateCookieParams();
+        return parent::regenerateId();
+    }
+
+    /**
+     * @return $this
+     */
+    protected function updateCookieParams()
+    {
+        $params = session_get_cookie_params();
+        if (!empty($params['secure']) && !empty($params['samesite']) && (strtolower($params['samesite']) === 'none')) {
+            return $this;
+        }
+
+        $params['secure'] = true;
+        $params['samesite'] = 'None';
+
+        session_set_cookie_params($params);
+        return $this;
+    }
+}
