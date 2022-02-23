9f5054c43da110c6a8096c5a06d889e39a63ea0d
Tolik Legkyi <>
2022-02-23 21:02:43 +0200

    POC-2661 cookie settings update

--- view/frontend/web/js/jquery.storageapi.extended.js
+++ view/frontend/web/js/jquery.storageapi.extended.js
@@ -2,32 +2,44 @@ define([
     'jquery',
     'jquery/jquery.cookie',
     'jquery/jquery.storageapi.min',
-    'Magento_Cookie/js/jquery.storageapi.extended'
+    'mage/cookies',
+    'Magento_Cookie/js/jquery.storageapi.extended',
 ], function ($) {
     'use strict';
 
     function _extend(storage) {
-
         var origSetConf = storage.setConf.bind(storage);
         var origSetItem = storage.setItem.bind(storage);
 
-        $.extend(storage, {
-            setItem: function (name, value, options) {
-                options = options || {};
-                options.samesite = 'None';
+        storage.setItem = function (name, value, options) {
+            options = options || {};
+            options.samesite = 'None';
+            //
+            return origSetItem(name, value, options);
+        };
 
-                return origSetItem(name, value, options);
-            },
-            setConf: function (c) {
-                c = c || {};
-                c.samesite = 'None';
-
-                return origSetConf(c);
-            },
-        });
+        storage.setConf =  function (c) {
+            c = c || {};
+            c.samesite = 'None';
+            //
+            return origSetConf(c);
+        };
     };
 
     if (window.cookieStorage) {
         _extend(window.cookieStorage);
+    }
+
+    if ($.mage && $.mage.cookies) {
+        var origSet = $.mage.cookies.set.bind($.mage.cookies);
+
+        $.extend($.mage.cookies, {
+            set: function (name, value, options) {
+                options = options || {};
+                options.samesite = 'None';
+                //
+                return origSet(name, value, options);
+            }
+        });
     }
 });
