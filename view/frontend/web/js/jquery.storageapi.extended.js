08e3f18c489bd5cae0ef5703ccdc6001d7c69e9f
Tolik Legkyi <>
2022-03-18 18:21:57 +0200

    support 2.2.x version on frontend

--- view/frontend/web/js/jquery.storageapi.extended.js
+++ view/frontend/web/js/jquery.storageapi.extended.js
@@ -2,50 +2,96 @@ define([
     'jquery',
     'jquery/jquery.cookie',
     'jquery/jquery.storageapi.min',
-    'mage/cookies',
-    'Magento_Cookie/js/jquery.storageapi.extended',
+    'mage/cookies'
+    //'Magento_Cookie/js/jquery.storageapi.extended',
 ], function ($) {
     'use strict';
 
     console.info('poc jquery cookies init');
 
+    //almost fully-duplicated (with changes) part of code from 2.4.3 for
+    //CE/EE below 2.4.3
+    /**
+     *
+     * @param {Object} storage
+     * @private
+     */
     function _extend(storage) {
-        //keep it consistent with CE/EE 2.4.3+
-        storage._samesite = 'None';
-        storage._secure = true;
-
-        //almost fully-duplicated (with changes) part of code from 2.4.3 for
-        //CE/EE below 2.4.3
-        storage.setItem = function (name, value, options) {
+        //var cookiesConfig = window.cookiesConfig || {};
+        $.extend(storage, {
             //-----------------------------------//
             //hardcoded values
-            options = options || {};
-            options.samesite = 'None';
-            options.secure = true;
+            _secure: true,
+            _samesite: 'None',
             //-----------------------------------//
 
-            var _default = {
-                expires: this._expires,
-                path: this._path,
-                domain: this._domain,
-                secure: this._secure,
-                storage: this._samesite
-            };
+            /**
+             * Set value under name
+             * @param {String} name
+             * @param {String} value
+             * @param {Object} [options]
+             */
+            setItem: function (name, value, options) {
+                //-----------------------------------//
+                //hardcoded values
+                options = options || {};
+                options.samesite = 'None';
+                options.secure = true;
+                //-----------------------------------//
+                var _default = {
+                    expires: this._expires,
+                    path: this._path,
+                    domain: this._domain,
+                    secure: this._secure,
+                    samesite: this._samesite
+                };
 
-            $.cookie(this._prefix + name, value, $.extend(_default, options || {}));
-        };
+                $.cookie(this._prefix + name, value, $.extend(_default, options || {}));
+            },
+
+            /**
+             * Set default options
+             * @param {Object} c
+             * @returns {storage}
+             */
+            setConf: function (c) {
+                //-----------------------------------//
+                //hardcoded values
+                c = c || {};
+                c.samesite = 'None';
+                c.secure = true;
+                //-----------------------------------//
+                if (c.path) {
+                    this._path = c.path;
+                }
+
+                if (c.domain) {
+                    this._domain = c.domain;
+                }
+
+                if (c.expires) {
+                    this._expires = c.expires;
+                }
+
+                if (typeof c.secure !== 'undefined') {
+                    this._secure = c.secure;
+                }
+
+                if (typeof c.samesite !== 'undefined') {
+                    this._samesite = c.samesite;
+                }
+
+                return this;
+            }
+        });
     }
 
-    if (window.cookieStorage) {
-        _extend(window.cookieStorage);
-    }
-
-    if ($.mage && $.mage.cookies) {
+    function _extendMage() {
         //fully-duplicated part of code from 2.4.3 for
         //CE/EE below 2.4.3
         function lifetimeToExpires(options, defaults) {
             var expires,
-                lifetime;
+              lifetime;
 
             lifetime = options.lifetime || defaults.lifetime;
 
@@ -70,10 +116,10 @@ define([
                 //-----------------------------------//
 
                 var expires,
-                    path,
-                    domain,
-                    secure,
-                    samesite;
+                  path,
+                  domain,
+                  secure,
+                  samesite;
 
                 options = $.extend({}, this.defaults, options || {});
                 expires = lifetimeToExpires(options, this.defaults) || options.expires;
@@ -83,12 +129,20 @@ define([
                 samesite = options.samesite;
 
                 document.cookie = name + '=' + encodeURIComponent(value) +
-                    (expires ? '; expires=' + expires.toUTCString() :  '') +
-                    (path ? '; path=' + path : '') +
-                    (domain ? '; domain=' + domain : '') +
-                    (secure ? '; secure' : '') +
-                    '; samesite=' + (samesite ? samesite : 'lax');
+                  (expires ? '; expires=' + expires.toUTCString() :  '') +
+                  (path ? '; path=' + path : '') +
+                  (domain ? '; domain=' + domain : '') +
+                  (secure ? '; secure' : '') +
+                  '; samesite=' + (samesite ? samesite : 'lax');
             }
         });
+    }
+
+    if (window.cookieStorage) {
+        _extend(window.cookieStorage);
+    }
+
+    if ($.mage && $.mage.cookies) {
+        _extendMage();
     }
 });
